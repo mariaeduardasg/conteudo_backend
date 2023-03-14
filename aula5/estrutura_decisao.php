60060d89c666a0edac9e2a34c3d2e5fa72c1266b
Maria Eduarda <maria.gois@alunos.ifsuldeminas.edu.br>
2023-03-14 07:21:12 -0300

    Aula 7

--- aula5/estrutura_decisao.php
+++ aula5/estrutura_decisao.php
@@ -1,16 +1,15 @@
-<h2> Estrutura de decisão</h2>
+<h2>Estrutura de decisão</h2>
 <p>
     As estruturas de condição permitem executar
-    blocos distintos de código dado uma condição
+    blocos distintos de código dado uma condição.
 
     <strong>Exemplo</strong>
     <pre>
         if(condicao){
-            //intruções executadas
-            //caso a condição seja verdadeira 
-
+            //instruções executadas
+            //caso a condição seja verdadeira
         }else{
-            //instruções executadas, caso 
+            //instruções executadas, caso
             //a condição seja falsa.
         }
     </pre>
@@ -25,16 +24,55 @@
         echo "O aluno foi reprovado";
     }
 ?>
----------------------
-media >= 6      | aprovado
-6 < media >= 3  | exame 
-media < 3       | reprovado
+
+--------------------------------------
+10 < media >= 6  | aprovado
+6 < media >=     | exame
+media < 3        | reprovado
 <?php
-    $media = 7;
+    $media = 4;
 
     if($media >= 6){
         echo "O aluno foi aprovado";
+    }else if($media >= 3){
+        echo "O aluno ficou de recuperação";
     }else{
         echo "O aluno foi reprovado";
+    }
+?>
+
+<p>
+    O PHP faz a conversão automática de alguns tipos
+    para verdadeiro ou falso.
+
+    <strong>True</strong>
+    <ul>
+        <li>String com texto</li>
+        <li>Número maior que 0</li>
+        <li>Vetor com elementos</li>
+    </ul>
+
+    <strong>False</strong>
+    <ul>
+        <li>String vazia</li>
+        <li>Número igual a 0</li>
+        <li>Vetor vazio</li>
+    </ul>
+</p>
+<?php
+    if("o loco meu!"){
+        echo "verdadeiro";
+    }else {
+        echo "falso";
+    }
+
+?>
+
+<?php
+    $media = 4;
+    if($media >=6 || "Quero passar de ano"){
+        echo "Huhuhu, sou esperto";
+    }else{
+        echo "ihh, me dei mal";
     }
 ?>
