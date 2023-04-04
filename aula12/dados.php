3e761fdcbb4f3bb99b099ee4086f75c5238637cd
Duda Gois <maria.gois@alunos.ifsuldeminas.edu.br>
2023-04-04 08:46:55 -0300

    aula13

--- aula12/dados.php
+++ aula12/dados.php
@@ -7,56 +7,47 @@ $noticia1 = [
     "chamada" => "Aos 67 anos, Madalena Farias frequenta há mais de 20 anos as festas e costuma vender bombons, como forma de auxiliar na renda financeira.",
 ];
 
-$noticia2 = [
-    "titulo" => "'Vovó do trance' faz sucesso em festas eletrônicas de Belém: 'aproveito a chance de fazer o que não fiz quando era jovem'",
-    "materia" => "Frequentadora há mais de 20 anos das festas de música eletrônica, Madalena Farias, de 67 anos, mora no bairro da Cidade Nova, em Ananindeua, região metropolitana de Belém.",
-    "foto" => "https://s2.glbimg.com/RD8DPN7qZ4nndlTw2ikzstkwzPs=/0x0:960x1280/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/x/g/DNNvCpTGa7nGLAiKupAQ/whatsapp-image-2023-03-28-at-14.38.03.jpeg",
-    "chamada" => "Aos 67 anos, Madalena Farias frequenta há mais de 20 anos as festas e costuma vender bombons, como forma de auxiliar na renda financeira.",
-];
 
-$noticia3 = [
+$noticia2 = [
     "titulo" => "Assédio moral e assédio sexual: entenda como reconhecer agressões no ambiente de trabalho",
     "materia" => "O assédio sexual é toda conduta de natureza sexual exercida sobre alguém sem seu consentimento e com restrição à sua liberdade de dizer não. São atos que atingem a honra, a dignidade e a moral da vítima.",
     "foto" => "https://s2.glbimg.com/x9TNoa9K8Vh4TsQUlPPuhqi8Ka8=/0x0:620x465/984x0/smart/filters:strip_icc()/s.glbimg.com/jo/g1/f/original/2015/06/16/bdms-assedio-moral.jpg",
     "chamada" => "Cartilha publicada pelo Tribunal Superior do Trabalho orienta vítimas e testemunhas."
 ];
 
-$noticia4 = [
+$noticia3 = [
     "titulo" => "Prefeito de Petrópolis, Rubens Bomtempo é alvo de operação contra suposta fraude em licitações",
     "materia" => "O prefeito de Petrópolis, Rubens Bomtempo (PSB), é alvo nesta quarta-feira (29) da Operação PII,    do Ministério Público do Rio de Janeiro (MPRJ) e da Polícia Civil do RJ, que investiga possível fraude em licitações     no município da Região Serrana fluminense.", 
     "foto" => "https://s2.glbimg.com/L7rPPKnX_uglI9Q_8zXduwE5ZIY=/0x0:824x463/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/f/A/iNrHo4T1ac6atmUROWJQ/petropolis.jpg",
     "chamada" => "Promotores cumpriram mandados na casa do prefeito e no gabinete dele. Força-tarefa também investiga empresários de ônibus."
    ];
   
-  $noticia5 = [
-    "titulo" => "'Vovó do trance' faz sucesso em festas eletrônicas de Belém: 'aproveito a chance de fazer o que não fiz quando era jovem'",
-    "materia" => "Frequentadora há mais de 20 anos das festas de música eletrônica, Madalena Farias, de 67 anos, mora no bairro da Cidade Nova, em Ananindeua, região metropolitana de Belém.",
-    "foto" => "https://s2.glbimg.com/RD8DPN7qZ4nndlTw2ikzstkwzPs=/0x0:960x1280/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/x/g/DNNvCpTGa7nGLAiKupAQ/whatsapp-image-2023-03-28-at-14.38.03.jpeg",
-    "chamada" => "Aos 67 anos, Madalena Farias frequenta há mais de 20 anos as festas e costuma vender bombons, como forma de auxiliar na renda financeira.",
-  ]; 
   
-  $noticia6 = [
+ 
+  
+  $noticia4 = [
         "titulo" => "Paula Fernandes causa espanto em fãs por rosto diferente em foto com cantor Samuel Rosa: 'Quase não reconheci'",
         "materia" => "Paula Fernandes causou espanto em seus fãs ao surgir com o rosto irreconhecível em foto com Samuel Rosa antes do último show do Skank no Mineirão, em Belo Horizonte. Muitos se perguntaram o que a cantora fez na face para que ficasse assim.",
         "foto" => "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA19dITz.img?w=768&h=451&m=6&x=435&y=36&s=411&d=115",
         "chamada" => "Paula Fernandes abre o jogo sobre mudança no rosto"
   ];
+
   
-  $noticia7 = [
+  $noticia5 = [
       "titulo" => "Assédio moral e assédio sexual: entenda como reconhecer agressões no ambiente de trabalho",
       "materia" => "O assédio sexual é toda conduta de natureza sexual exercida sobre alguém sem seu consentimento e com restrição à sua liberdade de dizer não. São atos que atingem a honra, a dignidade e a moral da vítima.",
       "foto" => "https://s2.glbimg.com/x9TNoa9K8Vh4TsQUlPPuhqi8Ka8=/0x0:620x465/984x0/smart/filters:strip_icc()/s.glbimg.com/jo/g1/f/original/2015/06/16/bdms-assedio-moral.jpg",
       "chamada" => "Cartilha publicada pelo Tribunal Superior do Trabalho orienta vítimas e testemunhas."
   ];
   
-  $noticia8 = [
+  $noticia6 = [
       "titulo" => "Filho de bilionário confessa à BBC assassinato de estudante em Londres em 2008",
       "materia" => "O filho de um bilionário, que fugiu para o Iêmen horas depois da morte de uma estudante em Londres, há 15 anos, admitiu à BBC News sua participação no assassinato dela.  ",
       "foto" => "https://s2.glbimg.com/TWIfaLQyt7kir5aJ0IRE3cWlsVk=/0x0:851x476/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2023/r/t/8RgooTRBamuJTBojRenQ/corpo-de-martine-vik-magnussen-foi-encontrado-em-2008.jpg",
       "chamada" => "Farouk Abdulhak, que está na lista de mais procurados da Polícia Metropolitana de Londres, é alvo de um mandado internacional de prisão. Ele nunca havia falado sobre o caso."
   ];
   
-  $noticia9 = [
+  $noticia7 = [
       "titulo" => "Blasfêmia cobra seu preço",
       "materia" => "Após blasfêmia com o todo poderoso, homem tem a audacia de escrever Deus com d minusculo. Após isso a ira do Senhor pairou sobre ele, o transformando numa criatura abominavel",
       "foto" => "https://twitter.com/NgmOdeiaIsso/status/1581128685805244416/photo/1",
@@ -64,7 +55,7 @@ $noticia4 = [
   ];
   
   
-  $noticias = [$noticia1,$noticia2,$noticia3,$noticia4,$noticia5,$noticia6,$noticia7,$noticia8,$noticia9];
+  $noticias = [$noticia1,$noticia2,$noticia3,$noticia4,$noticia5,$noticia6,$noticia7];
 
   ?>
 
