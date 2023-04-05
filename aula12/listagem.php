e49251f379d7e01278dc8428c5a1d6bb56a35bf3
Maria Eduarda <maria.gois@alunos.ifsuldeminas.edu.br>
2023-04-05 10:38:38 -0300

    aula14

--- aula12/listagem.php
+++ aula12/listagem.php
@@ -14,7 +14,7 @@
 
     <div class="row row-cols-1 row-cols-md-3 g-3">
 
-    <?php foreach($noticias as $noticia): ?>
+    <?php foreach($noticias as $id => $noticia): ?>
 
             <div class="col">
             <div class="card">
@@ -22,7 +22,7 @@
             <div class="card-body">
                 <h5 class="card-title"><?php echo $noticia["titulo"]?></h5>
                 <p class="card-text"><?php echo $noticia["chamada"]?></p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="ler.php?id=<?php echo $id ?>" class="btn btn-primary">Ler mais</a>
             </div>
         </div>
 
