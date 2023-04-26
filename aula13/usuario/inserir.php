931be7531d6186e4d18eee8787edeb6c7d910800
duda gois <maria.gois@alunos.ifsuldeminas.edu.br>
2023-04-26 10:39:42 -0300

    aula13

---
+++ aula13/usuario/inserir.php
@@ -0,0 +1,29 @@
+<?php
+   
+   //importa o arquivo de conexão
+   require_once "../conexao.php";
+
+   //pegar os valors enviados do formulário
+   $nome = $_POST["nome"];
+   $login =$_POST["login"];
+   $senha = $_POST["senha"];
+
+   //cria uma variável com um comando SQL
+   $SQL = "INSERT INTO `usuario` (`nome`, `login`, `senha`) VALUES ('$nome', '$login', '$senha');";
+
+   echo $SQL; 
+
+   //prepara o comando para ser executado no mysql
+   $comando = $conexao->prepare($SQL);
+
+   //executa o comando
+   $comando->execute();
+
+   
+
+
+
+
+
+
+
